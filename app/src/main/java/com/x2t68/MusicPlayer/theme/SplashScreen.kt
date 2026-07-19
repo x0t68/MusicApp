aa25f8dabeeab1bbe5eefbcde8cb9ecd9466f2b2
Taha <taha.x2t68@gmail.com>
2026-07-19 18:10:05 +0300

    improveUi

--- app/src/main/java/com/x2t68/MusicPlayer/theme/SplashScreen.kt
+++ app/src/main/java/com/x2t68/MusicPlayer/theme/SplashScreen.kt
@@ -1,7 +1,7 @@
 package com.x2t68.MusicPlayer.theme
 
 import android.content.Intent
-import android.net.Uri // <-- هذا هو الاستيراد الصحيح
+import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
